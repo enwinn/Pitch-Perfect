52b6ae2f963288db0d156a3f5db664e445be5bb0
Eric N. Winn <enwinn@gmail.com>
2015-03-31 22:36:29 -0400

    Cleanup repetitive code, change switch indents, add missing prepareToPlay, and add in some reverb. Reverb is temporary - I will move it into a playReverb button and also add a playEcho button after I confirm I can push to github.

--- Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -26,6 +26,7 @@ class PlaySoundsViewController: UIViewController, AVAudioPlayerDelegate {
         
         audioPlayer = AVAudioPlayer(contentsOfURL: receivedAudio.filePathUrl, error: nil)
         audioPlayer.enableRate = true
+        audioPlayer.prepareToPlay()
         
         audioEngine = AVAudioEngine()
         audioFile = AVAudioFile(forReading: receivedAudio.filePathUrl, error: nil)
@@ -44,26 +45,20 @@ class PlaySoundsViewController: UIViewController, AVAudioPlayerDelegate {
     }
     
     @IBAction func stopAudioPlay(sender: UIButton) {
-        audioPlayer.stop()
-        audioEngine.stop()
-        audioEngine.reset()
-        stopButton.enabled = false
+        stopAllAudio()
     }
     
     @IBAction func playChipmunkAudio(sender: UIButton) {
-        playAudioWithVariablePitch(1000, rate: 1.0, overlap: 8.0)
+        playAudioWithVariablePitch(1200, rate: 1.0, overlap: 8.0)
     }
     
     @IBAction func DarthVaderButton(sender: UIButton) {
-        playAudioWithVariablePitch(-900, rate: 1.0, overlap: 8.0)
+        playAudioWithVariablePitch(-800, rate: 1.0, overlap: 8.0)
     }
     
     func playAudioSpeed (speed: Float) {
-        var session = AVAudioSession.sharedInstance()
-        session.setCategory(AVAudioSessionCategoryPlayback, error: nil)
-        audioPlayer.stop()
-        audioEngine.stop()
-        audioEngine.reset()
+        setAudioSession()
+        stopAllAudio()
         
         // Always start at the beginning of the track
         audioPlayer.currentTime = 0.0
@@ -90,49 +85,52 @@ class PlaySoundsViewController: UIViewController, AVAudioPlayerDelegate {
     }
     
     func playAudioWithVariablePitch(pitch: Float, rate: Float, overlap: Float){
-        var session = AVAudioSession.sharedInstance()
-        session.setCategory(AVAudioSessionCategoryPlayback, error: nil)
-        audioPlayer.stop()
-        audioEngine.stop()
-        audioEngine.reset()
+        setAudioSession()
+        stopAllAudio()
         
         var audioPlayerNode = AVAudioPlayerNode()
         audioEngine.attachNode(audioPlayerNode)
         
         var changePitchEffect = AVAudioUnitTimePitch()
+        var changeReverbEffect = AVAudioUnitReverb()
+        
+        changeReverbEffect.loadFactoryPreset(.LargeChamber)
+        changeReverbEffect.wetDryMix = 20.0
         
         // Pitch is defined in cents. Default is 1.0. Range is -2400.0 to 2400.0 One octave is 1200 cents, one semitone is 100 cents
         switch pitch {
-        case -2400.0...2400.0:
-            changePitchEffect.pitch = pitch
-        default:
-            changePitchEffect.pitch = 1.0
+            case -2400.0...2400.0:
+                changePitchEffect.pitch = pitch
+            default:
+                changePitchEffect.pitch = 1.0
         }
         
         // Rate default is 1.0. Range is 1/32 to 32.0
         switch rate {
-        case 1/32...32/0:
-            changePitchEffect.rate = rate
-        default:
-            changePitchEffect.rate = 1.0
+            case 1/32...32/0:
+                changePitchEffect.rate = rate
+            default:
+                changePitchEffect.rate = 1.0
         }
         
         //Higher value results in fewer artifacts in output signal. Default value is 8.0. Range is 3.0 to 32.0
         switch overlap {
-        case 3.0...32.0:
-            changePitchEffect.overlap = overlap
-        default:
-            changePitchEffect.overlap = 8.0
+            case 3.0...32.0:
+                changePitchEffect.overlap = overlap
+            default:
+                changePitchEffect.overlap = 8.0
         }
         
         audioEngine.attachNode(changePitchEffect)
+        audioEngine.attachNode(changeReverbEffect)
         
         audioEngine.connect(audioPlayerNode, to: changePitchEffect, format: nil)
-        audioEngine.connect(changePitchEffect, to: audioEngine.outputNode, format: nil)
+        audioEngine.connect(changePitchEffect, to: changeReverbEffect, format: nil)
+        audioEngine.connect(changeReverbEffect, to: audioEngine.outputNode, format: nil)
         
         audioPlayerNode.scheduleFile(audioFile,
             atTime: nil,
-            completionHandler: {self.stopButton.enabled = false })
+            completionHandler: audioPlayerNodeCompletion)
         audioEngine.startAndReturnError(nil)
         
         stopButton.enabled = true
@@ -140,4 +138,25 @@ class PlaySoundsViewController: UIViewController, AVAudioPlayerDelegate {
         
     }
     
+    // ref: http://www.ios-developer.net/iphone-ipad-programmer/development/threads/updating-ui-controls-on-background-threads
+    // ref: Sameer pointed this out to me during our study group on 3/30/2015
+    func audioPlayerNodeCompletion() {
+        dispatch_async(dispatch_get_main_queue(), {
+            // Seems to fire a bit before audio ends, why?
+            self.stopButton.enabled = false
+        })
+    }
+    
+    func stopAllAudio() {
+        audioPlayer.stop()
+        audioEngine.stop()
+        audioEngine.reset()
+        stopButton.enabled = false
+    }
+    
+    func setAudioSession() {
+        AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback, error: nil)
+        AVAudioSession.sharedInstance().setActive(true, error: nil)
+    }
 }
+
